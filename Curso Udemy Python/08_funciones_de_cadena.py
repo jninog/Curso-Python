52319c08195695e0dc46086c3ea412a014f31f85
jninog <julininogon@gmail.com>
2023-03-01 17:32:49 -0500

    subir

---
+++ Curso Udemy Python/08_funciones_de_cadena.py
@@ -0,0 +1,13 @@
+cadena = 'Hola Mundo'
+print(len(cadena))
+cadenaMayus = cadena.upper()
+print(cadenaMayus)
+
+cadenaMinus = cadena.lower()
+print(cadenaMinus)
+
+print(cadena.split())
+
+cadenadiv ='uva,pera,manzana,naranja'
+print(cadenadiv.split(','))
+print(cadenadiv.split())
---
+++ Curso Udemy Python/09_imprimir_variable_de_una_cadena.py
@@ -0,0 +1,13 @@
+nombre = 'Antonio'
+edad = 18
+print('Buenos días '+ nombre)
+
+print('Buenos días {}, feliz {} cumpleaños'.format(nombre,edad))
+
+resultado = 10/3
+print(resultado)
+print('El resultado es {r}'.format(r=resultado))
+print('El resultado es {r:1.3}'.format(r=resultado))
+
+#f-string
+print(f'Buenos días {nombre}, feliz {edad} cumpleaños')
