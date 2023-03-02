a3fdb8fbf722c66eb871054028c44975630ef408
jninog <julininogon@gmail.com>
2023-03-02 10:15:24 -0500

    Subir

---
+++ Curso Udemy Python/18_Operadores_logicos.py
@@ -0,0 +1,16 @@
+numero1 = 10
+numero2 = 5
+numero3 = 7
+numero4 = 8
+
+print(numero1 < numero2)
+print(numero3 < numero4)
+
+print((numero1 > numero2)and(numero3 < numero4))
+
+##compa = (numero1 > numero2)
+#compa2 = (numero3 < numero4)
+print((numero1 < numero2)or(numero3 > numero4))
+
+print(not(numero3 < numero4))
+
---
+++ Curso Udemy Python/19_operadores_de_identidad.py
@@ -0,0 +1,11 @@
+frutas1 = ['manzana', 'pera']
+frutas2 = ['manzana', 'pera']
+frutas3 = frutas1
+
+print(frutas3 is frutas1)
+
+#adicionar elementos
+
+frutas3.append('Naranjas')
+print(frutas3 is frutas1)
+print(frutas1)
