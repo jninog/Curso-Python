3046c44df2c9a06a5bfe9f511875b58005b0bde4
jninog <julininogon@gmail.com>
2023-03-01 19:11:35 -0500

    subir

---
+++ Curso Udemy Python/16_Operadores_de_asignacion.py
@@ -0,0 +1,33 @@
+numero = 5
+numero2=6
+print(numero)
+
+#numero = numero + 4  -- Esto equivale a lo que sigue
+#print(numero)
+
+numero += 4
+print(numero)
+
+numero2 = numero2-3
+print(numero2)
+
+numero2-=3
+print(numero2)
+
+numero = numero *4
+print(numero)
+
+numero *=2
+print(numero)
+
+numero = numero /2
+print(numero)
+
+numero /= 2
+print(numero)
+
+numero = numero**2
+print(numero)
+
+numero **= 2
+print(numero)
