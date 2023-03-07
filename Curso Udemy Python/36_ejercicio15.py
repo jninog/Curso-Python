6dd7514bf35681341873c24ab9ac02e1eddc7ff8
jninog <julininogon@gmail.com>
2023-03-07 15:01:15 -0500

    Subir

---
+++ Curso Udemy Python/36_ejercicio15.py
@@ -0,0 +1,13 @@
+nota = 4
+trabajo_realizado = 'si'
+
+
+print('Ingrese su nota')
+nota = input()
+
+
+
+if (float(nota)>=4) and (trabajo_realizado=='si'):
+    print('Aprobado')
+else:
+    print('Suspendido')
---
+++ Curso Udemy Python/37_ejercicio16.py
@@ -0,0 +1,6 @@
+inicio = 1
+fin = 6
+
+while(inicio<fin):
+   print(inicio)
+  inicio = inicio+1
