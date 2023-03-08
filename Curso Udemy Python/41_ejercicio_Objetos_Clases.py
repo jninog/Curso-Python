ce6d74d6c2a8ff7ce9329a9ef2f256e54d28d0f3
jninog <julininogon@gmail.com>
2023-03-08 10:06:26 -0500

    Subir

--- Curso Udemy Python/41_ejercicio_Objetos_Clases.py
+++ Curso Udemy Python/41_ejercicio_Objetos_Clases.py
@@ -1,16 +1,25 @@
 class coche:
+    marca = ''
+    color = ''
+    combustible = ''
+    cilindrada = ''
+    
     def __init__(self,marca,color,combustible, cilindrada):
         self.marca = marca
         self.color = color
         self.combustible = combustible
         self.cilindrada = cilindrada
-    def mostrar_caracteristicas(self):    
+    def mostrar_caracteristicas(self):    #instancio la clase completa para poder acceder a los atributos
         print('El carro tiene estas caracteristicas marca {},color {},Combustible {},cilindrada{}'
               .format(self.marca,self.color,self.combustible,self.cilindrada))
 
-coche1 = coche('Opel','Azul','Gasolina','1,6')
 
+coche1 = coche('Opel','Azul','Gasolina','1,6')
+coche2 = coche('1','1','1','1')
+
+print(coche2.mostrar_caracteristicas())
 print(coche1.marca)
 
 print(coche1.mostrar_caracteristicas())
+    
     
