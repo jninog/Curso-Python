6fe154c7e85e889162e51fe79dff4c66837220c3
jninog <julininogon@gmail.com>
2023-04-13 08:48:37 -0500

    Subir

--- Curso_Udemy_Python/79_try_ejercicio.py
+++ Curso_Udemy_Python/79_try_ejercicio.py
@@ -1,7 +1,7 @@
 try:
-    numero1 = 5
-    numero2 = 4
-    numero3 = 2
+    numero1 = 6
+    numero2 = 3
+    numero3 = 3
     resta = numero2 - numero3
     operacion = numero1/resta
     print(operacion)
@@ -9,3 +9,14 @@ except ZeroDivisionError:
     print("Error, division entre cero")
 except:
     print("Ha habido un error")
+    
+    
+def operacion(numero1,numero2,numero3):
+    resultado = numero1 / (numero2-numero3)
+    return(resultado)
+    
+try:
+    resultado = operacion(numero1,numero2,numero3)
+    print(resultado)
+except ZeroDivisionError:
+    print("Error, Los dos ultimos numero no pueden ser iguales")
