c46e2a4b58ebe35463ba2ef79f515fbb6ace72d5
jninog <julininogon@gmail.com>
2023-04-25 08:36:42 -0500

    subir

--- Curso_Udemy_Python/135_series.py
+++ Curso_Udemy_Python/135_series.py
@@ -21,3 +21,25 @@ print(name)
 
 serie_notas_daniel.index.name = 'Asignaturas Daniel' # poner nombre a los indices
 print(serie_notas_daniel)
+
+#convertir serie en diccionario
+
+diccionario = serie_notas_daniel.to_dict()
+print(diccionario)
+
+serie = pd.Series(diccionario)
+print(serie)
+
+#realizar operaciones entre series
+
+asignaturas = ['matematicas','historia','fisica','literatura']
+notas_ana= [7,8,5,9]
+serie_notas_ana = pd.Series(notas_ana,index=asignaturas)
+print(serie_notas_ana)
+
+
+serie_notas_clase = (serie_notas_daniel+serie_notas_ana)/2
+serie_notas_clase.name = 'Asignaturas curso' # poner nombre a los indices
+serie_notas_clase.index.name = 'Asignaturas promedio' # poner nombre a los indices
+#print(serie_notas_clase)
+print(serie_notas_clase)
